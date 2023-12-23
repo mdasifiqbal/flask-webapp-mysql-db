4bb49612f4f955e7b8cacd8ec25bfee30d51a683
John Aziz <johnaziz269@gmail.com>
2023-12-23 19:41:49 +0200

    Update azure_production.py

--- environment/azure_production.py
+++ environment/azure_production.py
@@ -1,11 +1,8 @@
 import os
 
-conn_str = os.environ.get('AZURE_MYSQL_CONNECTIONSTRING')
-conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
-
 DATABASE_URI = 'mysql+pymysql://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-    dbuser=conn_str_params['user'],
-    dbpass=conn_str_params['password'],
-    dbhost=conn_str_params['host'],
-    dbname=conn_str_params['dbname']
+    dbuser=os.environ.get('AZURE_MYSQL_USER'),
+    dbpass=os.environ.get('AZURE_MYSQL_PASSWORD'),
+    dbhost=os.environ.get('AZURE_MYSQL_HOST'),
+    dbname=os.environ.get('AZURE_MYSQL_NAME')
 )
