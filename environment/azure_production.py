9b1daebfe06ede59c1f2bf0fde3df7696e5412c0
John A <johnaziz269@gmail.com>
2023-12-24 19:22:47 +0200

    fix: add ssl certificate

--- environment/azure_production.py
+++ environment/azure_production.py
@@ -1,6 +1,7 @@
 import os
 
-DATABASE_URI = 'mysql+pymysql://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
+# mysql+pymysql://your-username:your-password@localhost/your-schema?ssl_ca=BaltimoreCyberTrustRoot.crt.pem'
+DATABASE_URI = 'mysql+pymysql://{dbuser}:{dbpass}@{dbhost}/{dbname}?ssl_ca=DigiCertGlobalRootG2.crt.pem&ssl_disabled=false'.format(
     dbuser=os.environ.get('AZURE_MYSQL_USER'),
     dbpass=os.environ.get('AZURE_MYSQL_PASSWORD'),
     dbhost=os.environ.get('AZURE_MYSQL_HOST'),
