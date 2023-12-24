ce79527c6148657300d5c3667ffc7f678024de7a
John A <johnaziz269@gmail.com>
2023-12-24 19:37:52 +0200

    fix: remove ssl required

--- environment/azure_production.py
+++ environment/azure_production.py
@@ -1,7 +1,7 @@
 import os
 
 # mysql+pymysql://your-username:your-password@localhost/your-schema?ssl_ca=BaltimoreCyberTrustRoot.crt.pem'
-DATABASE_URI = 'mysql+pymysql://{dbuser}:{dbpass}@{dbhost}/{dbname}?ssl_ca=DigiCertGlobalRootG2.crt.pem&ssl_disabled=false'.format(
+DATABASE_URI = 'mysql+pymysql://{dbuser}:{dbpass}@{dbhost}/{dbname}?ssl_ca=DigiCertGlobalRootG2.crt.pem'.format(
     dbuser=os.environ.get('AZURE_MYSQL_USER'),
     dbpass=os.environ.get('AZURE_MYSQL_PASSWORD'),
     dbhost=os.environ.get('AZURE_MYSQL_HOST'),
